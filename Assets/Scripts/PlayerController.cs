147d9cfe7b988f2e9c0ae553ef5ffae419fb352b
LAPTOP-T0STR9M7\Akhil mohan <akhilmohan97@gmail.com>
2020-07-10 00:23:46 +0530

    added player input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        Vector3 scal = transform.localScale;
+        if (speed == 1)
+        {
+            scal.x = 2;
+            animator.SetBool("speed greater", true);
+            transform.localScale = scal;
+           
+        }
+       else if (speed < 0)
+        {
+            scal.x = -2;
+          
+            animator.SetBool("speed greater", true);
+           
+           transform.localScale = scal;
+        }
+       else 
+        {
+              animator.SetBool("speed greater", false);
+           }
+        
+    }
+    
+}
