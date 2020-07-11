1fa0d23731e6ed06baddde8a7d4ef78c4f902704
LAPTOP-T0STR9M7\Akhil mohan <akhilmohan97@gmail.com>
2020-07-11 16:40:53 +0530

    level endimg amd new level is loaded

---
+++ Assets/LevelEndScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelEndScript : MonoBehaviour
+{
+    public string scene;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      if  (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+
+}
